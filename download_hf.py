92abd182ccc9dad35b9cab330689ad18cabf4d4e
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 <1123649556@qq.com>
2024-09-19 15:46:44 +0800

    update model download.py

---
+++ download_hf.py
@@ -0,0 +1,15 @@
+import transformers
+import os
+
+### make dir
+os.makedirs('hugging_cache', exist_ok=True)
+
+### download model
+models=["meta-llama/Llama-2-7b-hf","mistralai/Mistral-7B-v0.1"]
+TOKEN='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'
+
+for model in models:
+    print(f"Downloading {model}")
+    model_name=model.split("/")[-1]
+    transformers.AutoModel.from_pretrained(model,force_download=True,use_auth_token=TOKEN).save_pretrained(f"hugging_cache/{model_name}")
+    transformers.AutoTokenizer.from_pretrained(model,force_download=True,use_auth_token=TOKEN).save_pretrained(f"hugging_cache/{model_name}")
